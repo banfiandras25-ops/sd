83fda4f3e80696ec4b05d6327b557a5fa84bbdda
banfiandras25-ops <banfi.andras25@gmail.com>
2026-05-22 01:52:00 +0200

    Munka feltöltése

---
+++ Source/sign-in.php
@@ -0,0 +1,94 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = "localhost:3306";
+$user = "rh69626_admin";
+$pass = "admin222!!";
+$db = "rh69626_allatok";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Hiba: " . $conn->connect_error);
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+/*
+    Find user by email
+*/
+$stmt = $conn->prepare("
+    SELECT id, felhasznalonev, emailcim, jelszo, is_admin
+    FROM felhasznalok
+    WHERE emailcim = ?
+");
+
+$stmt->bind_param("s", $email);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($result->num_rows === 1) {
+
+    $user = $result->fetch_assoc();
+
+    /*
+        Plain text password check
+    */
+    if ($password === $user['jelszo']) {
+
+        // COOKIES (30 days)
+        setcookie(
+            "user_id",
+            $user['id'],
+            time() + (86400 * 30),
+            "/"
+        );
+
+        setcookie(
+            "username",
+            $user['felhasznalonev'],
+            time() + (86400 * 30),
+            "/"
+        );
+
+        setcookie(
+            "email",
+            $user['emailcim'],
+            time() + (86400 * 30),
+            "/"
+        );
+
+        setcookie(
+            "is_admin",
+            $user['is_admin'],
+            time() + (86400 * 30),
+            "/"
+        );
+
+        echo "Sikeres bejelentkezés!";
+
+        echo '
+        <script>
+            setTimeout(function() {
+                window.location.href = "/";
+            }, 2000);
+        </script>
+        ';
+
+    } else {
+        echo "Hibás jelszó!";
+    }
+
+} else {
+    echo "Nincs ilyen email cím!";
+}
+
+$stmt->close();
+$conn->close();
+
+?>
